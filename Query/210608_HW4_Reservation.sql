626e49ae09d66d3109b7fbb3d4d88aacdc5a844b
Ahnkyuwon504 <spider504@naver.com>
2021-06-08 20:32:01 +0900

    implement : reservation

--- Query/210608_HW4_Reservation.sql
+++ Query/210608_HW4_Reservation.sql
@@ -7,9 +7,10 @@
 ## 25번 슬라이드
 ## 리조트 예약시스템
 #########################################################################
+set sql_safe_updates=0;
 
 show databases;
-use kopoctc_HW4;
+use reservation;
 show tables;
 
 drop table if exists reservation;
@@ -26,6 +27,11 @@ create table reservation (
 desc reservation;
 
 delete from reservation where room>0;
-insert into reservation values ('헤헤', '20210510', 1, '헤헤', '010-9973-6907', '입금', '메모', '20190330');
+insert into reservation values ('트와이스', '20210611', 1, '헤헤', '010-9973-6907', '입금', '메모', '20190330');
+insert into reservation values ('원더걸스', '20210612', 1, '꼬꼬', '010-9973-6907', '입금', '메모', '20190330');
+insert into reservation values ('비투비', '20210615', 3, '나나', '010-9973-6907', '입금', '메모', '20190330');
+insert into reservation values ('김창렬', '20210613', 2, '텔레토비', '010-9973-6907', '입금', '메모', '20190330');
+insert into reservation values ('교수님', '20210610', 2, '텔레토비', '010-9973-6907', '입금', '메모', '20190330');
+insert into reservation values ('교수님', '20210608', 2, '텔레토비', '010-9973-6907', '입금', '메모', '20190330');
 select * from reservation;
 
